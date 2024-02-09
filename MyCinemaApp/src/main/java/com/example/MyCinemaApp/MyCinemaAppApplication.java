2fe9f58075e8460c2eec0302dfb7bc14da878eb0
grishagr9 <grisha-rnd10@yandex.ru>
2024-02-09 23:12:45 +0300

    Initial commit

---
+++ MyCinemaApp/src/main/java/com/example/MyCinemaApp/MyCinemaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyCinemaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCinemaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCinemaAppApplication.class, args);
+	}
+
+}
---
+++ MyCinemaApp/src/test/java/com/example/MyCinemaApp/MyCinemaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyCinemaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCinemaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
